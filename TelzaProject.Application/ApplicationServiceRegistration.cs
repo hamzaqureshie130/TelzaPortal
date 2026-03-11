50c2ef9499dc57375fc0aa77a51ebe881101adf6
mudassar96 <mudassarmehdi96@gmail.com>
2026-03-11 15:19:36 +0500

    Add project files.

---
+++ TelzaProject.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelzaProject.Application
+{
+    internal class ApplicationServiceRegistration
+    {
+    }
+}
